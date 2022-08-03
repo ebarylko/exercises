cc0fdfd4650ea033b30f04641c0c32f88d052d1f
Eitan Barylko <eitan@barylko.com>
2022-08-02 21:07:36 -0700

    Refactored next-genration function to use frequencies

--- src/clojure_exercises/conway.clj
+++ src/clojure_exercises/conway.clj
@@ -9,34 +9,46 @@
                 :when (not= pos [x y])]
             [x y]))
 
-(defn count-neighbours
-  "Pre: takes a board and a position on the board
-  Post: returns the number of neighbours from that position"
-  [board pos]
-  (->> pos
-       generate-neighbors
-       (filter board)
-       count))
+
+;; (defn apply-rules*
+;;   "Pre: takes a board and a position
+;;   Post: if the position is alive after applying the rules, returns a board with that pos, else none"
+;;   [old-board new-board pos]
+;;   (let [neighbors ((frequencies (mapcat generate-neighbors old-board)) pos)
+;;         alive? (old-board pos)]
+;;     (cond-> new-board
+;;       (or (= neighbors 3)
+;;           (and alive? (= 2 neighbors))) (conj pos))))
+
+;; (defn next-generation
+;;   "Pre: takes a board 
+;;   Post: returns the board after applying the rules to every position"
+;;   [board]
+;;   (reduce (partial apply-rules board) #{} (mapcat generate-neighbors board)))
+
 
 (defn apply-rules
-  "Pre: takes a board and a position
-  Post: if the position is alive after applying the rules, returns a board with that pos, else none"
-  [old-board new-board pos]
-  (let [neighbors (count-neighbours old-board pos)
-        alive? (old-board pos)]
-    (cond-> new-board
+  "Pre:takes, a map of key-values of position and the amount of neighbors, a new board, and a position
+  Post: if the position is alive after applying the rules, returns new board with that pos, else new-board"
+  [old-board new-board [pos neighbors]]
+(cond-> new-board
       (or (= neighbors 3)
-          (and alive? (= 2 neighbors))) (conj pos))))
+          (and (old-board pos) (= 2 neighbors))) (conj pos)))
 
 
 (defn next-generation
   "Pre: takes a board 
   Post: returns the board after applying the rules to every position"
   [board]
-  (reduce (partial apply-rules board) #{} (mapcat generate-neighbors board)))
+  (reduce
+  (partial apply-rules board)
+   #{}
+   (frequencies (mapcat generate-neighbors board))))
+
 
 (defn next-generations
   "Pre: takes a board and a number N
   Post: returns the board after N generations"
   [board n]
   (nth (iterate next-generation board) n))
+
